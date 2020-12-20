3a189e3f6a256983db285b2c816e40bebf158707
Chris baird <chrisbaird05@icloud.com>
2020-12-20 12:15:09 -0800

    Refactored to use IIFE
    nuff said.

--- revealing-module-pattern.js
+++ revealing-module-pattern.js
@@ -1,5 +1,5 @@
 // Revealing Module Pattern
-const myModule = () => {
+const myModule = (() => {
   let name = null || "No Name";
 
   return {
@@ -10,14 +10,11 @@ const myModule = () => {
       name = userName;
     },
   };
-};
+})();
 
-// Creating a instance of myModule
-const user = myModule();
-
-user.getName(); // "No Name"
+myModule.getName(); // "No Name"
 
 //Using exposed method to set name
-user.setName("Chris");
+myModule.setName("Chris");
 
-user.getName(); // "Chris"
+myModule.getName(); // "Chris"
